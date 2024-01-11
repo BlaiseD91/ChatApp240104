b5a119c3fb493a6fca852dd1e7cfedcced7e3c29
Székely Balázs <szekelyb91@gmail.com>
2024-01-11 17:06:54 +0100

    changed database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,34 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCGH6eMvLtt61DIMohtq0SWS4eurlnyaFw",
-    authDomain: "chat-83453.firebaseapp.com",
-    databaseURL: "https://chat-83453-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "chat-83453",
-    storageBucket: "chat-83453.appspot.com",
-    messagingSenderId: "658389265386",
-    appId: "1:658389265386:web:a78693dbf97d69a033e093",
-    measurementId: "G-PVR7TD07CF"
+
+  //ATTILA
+  firebaseConfig : {
+    apiKey: "AIzaSyC3Bz3BBGF8UrpUL3RDzXtMYCdRid21fG4",
+    authDomain: "chat-3c878.firebaseapp.com",
+    databaseURL: "https://chat-3c878-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-3c878",
+    storageBucket: "chat-3c878.appspot.com",
+    messagingSenderId: "835394735770",
+    appId: "1:835394735770:web:6915503c32ce637b7b5923",
+    measurementId: "G-GZS2YHX9S2"
   }
+
+  // SAJÁT
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyCGH6eMvLtt61DIMohtq0SWS4eurlnyaFw",
+  //   authDomain: "chat-83453.firebaseapp.com",
+  //   databaseURL: "https://chat-83453-default-rtdb.europe-west1.firebasedatabase.app",
+  //   projectId: "chat-83453",
+  //   storageBucket: "chat-83453.appspot.com",
+  //   messagingSenderId: "658389265386",
+  //   appId: "1:658389265386:web:a78693dbf97d69a033e093",
+  //   measurementId: "G-PVR7TD07CF"
+  // }
 };
 
 /*
